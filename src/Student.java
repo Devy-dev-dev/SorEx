33a0d050dcecf3cb567598a10aae7ff7f7689dd1
SteveRey <gtsteve.rey@gmail.com>
2020-01-08 14:20:55 +0100

    remplissage de la classe

--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,16 @@ package src;
 
 public class Student {
     /* possède les attributs suivants :
-    - date naissance :
+    - statut : OK, BANNED
+    - hash nom
+    - hash prenom
+    - hash date naissance
     - hash carte d'identité
     - hash JAPD
-    - arrayList de diplômes : [diplôme, date, université
+    - arrayList de diplômes : [cursus diplôme,
+                               nom diplôme,
+                               date,
+                               université]
 
      */
 }
