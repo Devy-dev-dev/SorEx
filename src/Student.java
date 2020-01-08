6b33bfa11af8a5f7bb33fe9e198bbafa6105d95b
SteveRey <gtsteve.rey@gmail.com>
2020-01-08 14:27:41 +0100

    remplissage de la classe

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,25 @@
 package src;
 
+import java.time.LocalDate;
+
 public class Student {
     /* possède les attributs suivants :
     - statut : OK, BANNED
-    - hash nom
-    - hash prenom
-    - hash date naissance
+    - nom
+    - prenom
+    - date naissance
     - hash carte d'identité
     - hash JAPD
     - arrayList de diplômes : [cursus diplôme,
                                nom diplôme,
                                date,
                                université]
-
      */
+
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+
+
+
 }
