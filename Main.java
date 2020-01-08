f7edf2672aaeb995576252db231b4a206244bbb2
SteveRey <gtsteve.rey@gmail.com>
2020-01-08 15:14:00 +0100

    remplissage de la classe Student

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import src.Student;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Marche avec Intellij");
+
+        Student s = new Student();
+        System.out.println(s.getNom());
+        System.out.println(s.getDateNaissance());
+        System.out.println(s.getHashID());
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,14 @@
 package src;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -14,15 +23,128 @@ public class Student {
     - arrayList de diplômes : [cursus diplôme,
                                nom diplôme,
                                date,
-                               université]
+                               université,
+                               moyenne]
      */
 
     private String nom;
     private String prenom;
-    private LocalDate dateNaissance;
-    String hashID;
-    String hashJAPD;
-    String[] diplomesDetail;
-    ArrayList<String> diplomes;
+    private LocalDate dateNaissance;  // https://www.baeldung.com/java-8-date-time-intro
+    private String hashID;
+    private String hashJAPD;
+    private String[] diplomesDetail = new String[5];
+    private ArrayList<String> diplomes;
 
+
+    // test que ça marche bien avec des valeurs exemples
+    public Student() {
+        this.nom = "Albert";
+        this.prenom = "Dupont";
+        this.dateNaissance = LocalDate.of(1994, 2, 20);
+        exempleID(); // genere le SHA de l'image
+
+    }
+
+    private void exempleID() {
+        // image :
+        File input = new File("src/documentsStudent/ID.png");
+        try {
+            // try to load the ID png image
+            BufferedImage buffImg = ImageIO.read(input);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write(buffImg, "png", outputStream);
+            byte[] IDdata = outputStream.toByteArray();
+
+            // try to convert hash the IDdata from image
+            this.hashID = hashingFunction(IDdata);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private String hashingFunction(byte[] IDdata) {
+        try {
+            // getInstance() method is called with algorithm SHA-512
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            // digest() method is called
+            // to calculate message digest of the input png
+            byte[] messageDigest = md.digest(IDdata);
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            StringBuilder hashtext = new StringBuilder(no.toString(16));
+
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32)
+                hashtext.insert(0, "0");
+
+            return hashtext.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getHashID() {
+        return hashID;
+    }
+
+    public void setHashID(String hashID) {
+        this.hashID = hashID;
+    }
+
+    public String getHashJAPD() {
+        return hashJAPD;
+    }
+
+    public void setHashJAPD(String hashJAPD) {
+        this.hashJAPD = hashJAPD;
+    }
+
+    public String[] getDiplomesDetail() {
+        return diplomesDetail;
+    }
+
+    public void setDiplomesDetail(String[] diplomesDetail) {
+        this.diplomesDetail = diplomesDetail;
+    }
+
+    public ArrayList<String> getDiplomes() {
+        return diplomes;
+    }
+
+    public void setDiplomes(ArrayList<String> diplomes) {
+        this.diplomes = diplomes;
+    }
 }
+
