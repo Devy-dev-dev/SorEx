e76cc05786bfb205d59ec2b58e8fa8d7a44d0e8b
SteveRey <gtsteve.rey@gmail.com>
2020-01-08 14:17:18 +0100

    ajout Client et Serveur

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Student {
+    /* possède les attributs suivants :
+    - date naissance :
+    - hash carte d'identité
+    - hash JAPD
+    - arrayList de diplômes : [diplôme, date, université
+
+     */
+}
