bb46cae3d39e1c52e1301d43eee0a232dcd9f9ce
SteveRey <gtsteve.rey@gmail.com>
2020-01-09 13:00:38 +0100

    ajout images au repo

--- src/StudentClient.java
+++ src/StudentClient.java
@@ -1,5 +1,21 @@
 package src;
 
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+
 public class StudentClient {
+//    String adresse = "localhost";
+//    final static int port = 8080;
+//
+//
+//    public StudentClient() throws IOException {
+//        Socket c = new Socket(adresse, port);
+//        c.connect("127.0.0.1", 8080);
+//    }
 
 }
