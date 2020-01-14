99a57f981de41ec52df19c2d927bea3766282772
SteveRey <gtsteve.rey@gmail.com>
2020-01-14 13:05:38 +0100

    maj class blockchain

--- Main.java
+++ Main.java
@@ -40,126 +40,57 @@ public class Main {
         copy.getBlock().get(1).getStudent().addDiplomes(diplomes);
         copy.getBlock().get(1).getStudent().addDiplomes(diplomes);
         copy.getBlock().get(1).getStudent().addDiplomes(diplomes);
-        System.out.println("b : ");
         b.addDiplomaFromUpdatedBlockchain(copy);
-        System.out.println(b);
-        System.out.println("\n\n\ncopy : ");
-        System.out.println(copy);
-        System.out.println("b = copy ? : "+b.verifyAllBlockchain(copy));
+//        System.out.println("b : "+b);
+//        System.out.println("\n\n\ncopy : ");
+//        System.out.println(copy);
+//        System.out.println("b = copy ? : "+b.verifyAllBlockchain(copy));
 
 
         // TODO: rajouter possibilité préfecture de mettre à jour carte d'identité + nom et prénom
         // TODO: ajouter possibilité chiffrer addresse et clef privée --> récupérer adresse étudiant
 
+
         // protection de la ressource critique
         final Semaphore mutex = new Semaphore(1);
         try {
             mutex.acquire();
+            Blockchain bRead = new Blockchain();
+            bRead = bRead.retrieveBlockchainFromFile();
+//            System.out.println("bRead null : "+(bRead == null));
+//            System.out.println(bRead.toString());
+            bRead.writeBlockchain();
+            mutex.release();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        try {
+            mutex.acquire();
+            Blockchain bRead = new Blockchain();
+//            bRead = bRead.retrieveBlockchainFromFile();
+//            System.out.println("bRead null : "+(bRead == null));
+//            System.out.println(bRead.toString());
             b.writeBlockchain();
             mutex.release();
         }catch (InterruptedException e){
             e.printStackTrace();
         }
 
-        Blockchain bRead = retrieveBlockchainFromFile();
-
-        System.out.println("identique ? : "+b.verifyAllBlockchain(bRead));
-
-    }
-
-    public static Blockchain retrieveBlockchainFromFile(){
-        try {
-            File file = new File("blockchain.txt");
-            FileReader fr = new FileReader(file);   //reads the file
-            BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
-            StringBuilder sb = new StringBuilder();    //constructs a string buffer with no characters
-            String line;
-            String temp = "";   // utilisée pour remplir dans line l'ArrayList des blocs lus
-
-            // on récupère la taille de la bolckchain
-            line = br.readLine();
-            int taille = Integer.parseInt(line);
-            String[] donnees;   // chaque indice contient 1 bloc
-
-            // on lit le reste de la blockchain (sans la taille au début du fichier)
-            while ((line = br.readLine()) != null) {
-                if (line.charAt(0) == "[".charAt(0)) {
-                    temp = line;
-                    while (!temp.contains("DONE")) {
-                        temp = br.readLine();
-                        line += temp;
-                    }
-                }
-                sb.append(line);
-                sb.append("\n");
-
-            }
-            String blockchainText = sb.toString();
-            blockchainText = blockchainText.replace("][", "\n");
-            blockchainText = blockchainText.replace("[", "");
-            blockchainText = blockchainText.replace("]", "\n");
-            donnees = blockchainText.split("\n");
 
 
-            String[] blockchainArray = new String[taille];
-            int cpt = 0;
-            for(int i = 0; i < donnees.length; i++){
-                blockchainArray[cpt] = "";
-                while(!donnees[i].equals("DONE")){
-                    blockchainArray[cpt] += donnees[i]+"\n";
-                    i++;
-                }
-                cpt++;
-            }
 
-            Blockchain bTemp = null;
-            for(int i = 0; i < blockchainArray.length; i++){
-                String[] data = blockchainArray[i].split("\n");
-                int parcoursData = 10;
-                StringBuilder inlineformation = new StringBuilder();
-                while (parcoursData < data.length){
-                    inlineformation.append(data[parcoursData]).append("\n");
-                    parcoursData++;
-                }
-                inlineformation = new StringBuilder(inlineformation.toString().replace(",", ""));
-                String[] formation = inlineformation.toString().split("\n");
 
-                // 1. on créer un étudiant depuis les valeurs lues
-                Student s = new Student();
-                s = s.newStudentFromFile(
-                        data[4],
-                        data[5],
-                        data[6],
-                        data[7],
-                        data[8],
-                        data[9],
-                        formation
-                );
+//        bRead = bRead.retrieveBlockchainFromFile();
+//        System.out.println("bRead null : "+(bRead == null));
+//        System.out.println("HHHHHHHHHHHHHHHHHHHHHHHHH");
+//        System.out.println(bRead.toString());
 
-                // 2. on créer un block ensuite
-                MinimalBlock block = new MinimalBlock();
-                block = block.minimalBlockFromFile(
-                        Integer.parseInt(data[0]),
-                        data[1],
-                        s,
-                        data[2],
-                        data[3]
-                        );
 
-                // 3. on met a jour la blockchain
-                if(i == 0)
-                    bTemp = new Blockchain(block);
-                else
-                    bTemp.addBlock(block);
 
-            }
-            return bTemp;
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        return null;  // si il y a eu un probleme
+//        System.out.println("b = bRead ? : "+b.verifyAllBlockchain(bRead));
 
     }
+
 
 }
--- src/Blockchain.java
+++ src/Blockchain.java
@@ -1,9 +1,6 @@
 package src;
 
-import java.io.FileWriter;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -213,6 +210,7 @@ public class Blockchain{
 //            boolean exists = tempFile.exists();
 //            //utiliser condition quand on voudra implémenter le append
 
+
             FileWriter fileWriter = new FileWriter("blockchain.txt");
             PrintWriter printWriter = new PrintWriter(fileWriter);
             printWriter.println(this.blocks.size());
@@ -250,6 +248,107 @@ public class Blockchain{
           }
       }
     }
+
+    public Blockchain retrieveBlockchainFromFile(){
+        try {
+            File file = new File("blockchain.txt");
+            FileReader fr = new FileReader(file);   //reads the file
+            BufferedReader br = new BufferedReader(fr);  //creates a buffering character input stream
+            StringBuilder sb = new StringBuilder();    //constructs a string buffer with no characters
+            String line;
+            String temp = "";   // utilisée pour remplir dans line l'ArrayList des blocs lus
+
+            // on récupère la taille de la bolckchain
+            line = br.readLine();
+            if (line == null)  // si le fichier est vide
+                return new Blockchain();
+
+            int taille = Integer.parseInt(line);
+            String[] donnees;   // chaque indice contient 1 bloc
+
+            // on lit le reste de la blockchain (sans la taille au début du fichier)
+            while ((line = br.readLine()) != null) {
+                if (line.charAt(0) == "[".charAt(0)) {
+                    temp = line;
+                    while (!temp.contains("DONE")) {
+                        temp = br.readLine();
+                        line += temp;
+                    }
+                }
+                sb.append(line);
+                sb.append("\n");
+
+            }
+            String blockchainText = sb.toString();
+            blockchainText = blockchainText.replace("][", "\n");
+            blockchainText = blockchainText.replace("[", "");
+            blockchainText = blockchainText.replace("]", "\n");
+            donnees = blockchainText.split("\n");
+
+
+            String[] blockchainArray = new String[taille];
+            int cpt = 0;
+            for (int i = 0; i < donnees.length; i++) {
+                blockchainArray[cpt] = "";
+                while (!donnees[i].equals("DONE")) {
+                    blockchainArray[cpt] += donnees[i] + "\n";
+                    i++;
+                }
+                cpt++;
+            }
+
+            Blockchain bTemp = null;
+            for (int i = 0; i < blockchainArray.length; i++) {
+                String[] data = blockchainArray[i].split("\n");
+                int parcoursData = 10;
+                StringBuilder inlineformation = new StringBuilder();
+                while (parcoursData < data.length) {
+                    inlineformation.append(data[parcoursData]).append("\n");
+                    parcoursData++;
+                }
+                inlineformation = new StringBuilder(inlineformation.toString().replace(",", ""));
+                String[] formation = inlineformation.toString().split("\n");
+
+                // 1. on créer un étudiant depuis les valeurs lues
+                Student s = new Student();
+                s = s.newStudentFromFile(
+                        data[4],
+                        data[5],
+                        data[6],
+                        data[7],
+                        data[8],
+                        data[9],
+                        formation
+                );
+
+                // 2. on créer un block ensuite
+                MinimalBlock block = new MinimalBlock();
+                block = block.minimalBlockFromFile(
+                        Integer.parseInt(data[0]),
+                        data[1],
+                        s,
+                        data[2],
+                        data[3]
+                );
+
+                // 3. on met a jour la blockchain
+                if (i == 0)
+                    bTemp = new Blockchain(block);
+                else
+                    bTemp.addBlock(block);
+
+            }
+            return bTemp;
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;  // si il y a eu un probleme
+
+    }
+
+
 
 
 }
